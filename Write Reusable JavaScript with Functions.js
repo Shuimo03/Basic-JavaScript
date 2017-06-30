18ce021683d6ffa96ce14b86af55b5d99402b7ce
Cola <wu1998102@gmail.com>
2017-06-30 10:13:13 +0800

    Create Write Reusable JavaScript with Functions.js

---
+++ Write Reusable JavaScript with Functions.js
@@ -0,0 +1,23 @@
+function myFunction(){
+  console.log("Hi World");
+}
+myFunction();
+
+/*
+在 JavaScript 中，我们可以把代码的重复部分抽取出来，放到一个函数（functions）中。
+
+这是一个函数（function）的例子：
+
+function functionName() {
+  console.log("Hello World");
+}
+你可以通过函数名称functionName加上后面的小括号来调用这个函数（function），就像这样：
+
+functionName();
+
+每次调用函数时它会打印出消息的“Hello World”到开发的控制台上。所有的大括号之间的代码将在每次函数调用时执行。
+
+任务
+创建一个名为 myFunction 的函数，这个函数可以打印“Hi World”到开发控制台上。
+调用这个函数
+*/
