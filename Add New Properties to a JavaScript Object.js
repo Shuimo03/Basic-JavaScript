024f6152bb064139a32c487f4ab3b3f135c3520d
Cola <wu1998102@gmail.com>
2017-07-05 10:30:34 +0800

    Create Add New Properties to a JavaScript Object.js

---
+++ Add New Properties to a JavaScript Object.js
@@ -0,0 +1,35 @@
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// 初始化变量
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Free Code Camp Campers"]
+};
+
+// 请把你的代码写在这条注释以下
+
+myDog["bark"] = "woof";
+
+/*
+你也可以像更改属性一样给对象添加属性。
+
+看看我们是如何给ourDog添加 "bark"属性：
+
+ourDog.bark = "bow-wow";
+
+或者
+
+ourDog["bark"] = "bow-wow";
+
+任务
+给myDog添加一个"bark" 属性，设置它的值为狗的声音，例如："woof"。
+*/
