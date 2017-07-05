ff3f2045af67bde61854fc9f97d6da2bab042258
Cola <wu1998102@gmail.com>
2017-07-05 10:34:16 +0800

    Create Delete Properties from a JavaScript Object.js

---
+++ Delete Properties from a JavaScript Object.js
@@ -0,0 +1,18 @@
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Free Code Camp Campers"],
+  "bark": "woof"
+};
+
+delete myDog.tails;
+
+/*
+我们同样可以删除对象的属性，例如：
+
+delete ourDog.bark;
+
+任务
+删除myDog对象的"tails"属性。
+*/
