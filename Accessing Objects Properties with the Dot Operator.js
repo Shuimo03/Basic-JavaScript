03d9db1cdbae188cc32d829587eff186bf1b9c39
Cola <wu1998102@gmail.com>
2017-07-05 10:16:19 +0800

    Create Accessing Objects Properties with the Dot Operator.js

---
+++ Accessing Objects Properties with the Dot Operator.js
@@ -0,0 +1,25 @@
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+var hatValue = testObj.hat;     
+var shirtValue = testObj.shirt;    
+
+/*
+有两种方式访问对象属性，一个是点操作符(.)，一个是中括号操作符([])。
+
+当你知道属性的名称的时候，使用点操作符。
+
+这是一个使用点操作符读取对象属性的例子：
+
+var myObj = {
+  prop1: "val1",
+  prop2: "val2"
+};
+var prop1val = myObj.prop1; // val1
+var prop2val = myObj.prop2; // val2
+任务
+通过点操作符读取对象testObj，把hat的属性值赋给变量hatValue，把shirt的属性值赋给shirtValue。
+*/
