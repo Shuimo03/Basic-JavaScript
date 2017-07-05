4ef7412bf51dd29e7a1e9a1d2e61627f11594a75
Cola <wu1998102@gmail.com>
2017-07-05 10:14:45 +0800

    Create Build JavaScript Objects.js

---
+++ Build JavaScript Objects.js
@@ -0,0 +1,41 @@
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// 请只修改这条注释以下的代码
+
+var myDog = {
+  
+  "name":"wutong",
+  "legs":4,
+  "tails":1,
+  "friends":["everything!"]
+  
+};
+
+
+/*
+你之前可能听说过对象 object 。
+
+对象和数组很相似，数组是通过索引来访问和修改数据，对象是通过属性来访问和修改数据的。
+
+这是一个示例对象：
+
+var cat = {
+  "name": "Whiskers",
+  "legs": 4,
+  "tails": 1,
+  "enemies": ["Water", "Dogs"]
+};
+对象适合用来存储结构化数据，就和真实世界的对象一模一样，比如一只猫。
+
+任务
+创建一个叫做myDog的对象，它里面有这些属性： 名称"name"、 腿"legs", 尾巴"tails"、朋友"friends"。
+
+你可以设置对象属性为任何你想要的值，只要 "name" 是字符串、 "legs" 和 "tails" 是数字、"friends" 是数组。
+
+
+*/
