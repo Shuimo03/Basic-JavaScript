de3ac02626ac5b0b1e0f4379b9f60e659f86b203
Cola <wu1998102@gmail.com>
2017-07-10 20:19:48 +0800

    Create Iterate with JavaScript While Loops.js

---
+++ Iterate with JavaScript While Loops.js
@@ -0,0 +1,18 @@
+var myArray = [];
+var i = 0;
+while(i<5){
+  myArray.push(i);
+  i++;
+}
+/*
+另一种类型的 JavaScript 循环被称为while循环，因为它规定，当（while）条件为真，循环才会执行，反之不执行。
+
+var ourArray = [];
+var i = 0;
+while(i < 5) {
+  ourArray.push(i);
+  i++;
+}
+任务
+通过一个 while 循环，把从 0 到 4 的值添加到 myArray 中。
+*/
