219d3111431adb6f721606bd3abf0b8e5264f79d
Cola <wu1998102@gmail.com>
2017-07-10 19:10:11 +0800

    Create Introducing JavaScript Object Notation JSON.js

---
+++ Introducing JavaScript Object Notation JSON.js
@@ -0,0 +1,50 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true
+  },
+    {
+      "artist":"Daft Punk", 
+      "title":"Homework",
+      "release_year":1997,
+      "formats":[
+        
+        "CD",
+        "Cassette",
+        "LP"
+      ]
+    }
+];
+
+
+/*
+JavaScript Object Notation 简称 JSON，它使用JavaScript对象的格式来存储数据。JSON是灵活的，因为它允许 数据结构 是 字符串，数字，布尔值，字符串，和 对象 的任意组合。
+
+这里是一个JSON对象的示例：
+
+var ourMusic = [
+  {
+    "artist": "Daft Punk",
+    "title": "Homework",
+    "release_year": 1997,
+    "formats": [ 
+      "CD", 
+      "Cassette", 
+      "LP" ],
+    "gold": true
+  }
+];
+这是一个对象数组，并且对象有各种关于专辑的 详细信息。它也有一个嵌套的 formats 的数组。附加专辑记录可以被添加到数组的最上层。
+
+提示
+数组中有多个 JSON 对象的时候，对象与对象之间要用逗号隔开。
+
+任务
+添加一个新专辑到 myMusic 的JSON对象。添加 artist 和 title 字符串，release_year 数字和 formats 字符串数组。
+*/
